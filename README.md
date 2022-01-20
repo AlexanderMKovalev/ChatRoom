a8e6d99d1ad0f4f70e790589e94c1d87288d37b6
AlexanderMKovalev <90866384+AlexanderMKovalev@users.noreply.github.com>
2022-01-20 10:34:27 -0500

    Copying project over
    Copying over project from old github account on alexander.m.kovalev@gmail.com.

---
+++ README.md
@@ -0,0 +1,28 @@
+# ChatRooms
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Usage](#Usage)
+* [Licence](#Licence)
+* [Live demo](#live-demo)
+
+## General info
+This project is an proof of concept web chat room designed with Microsoft .net core SignalR. Features includes: massages sent with-in rooms only and message typing indicator.
+	
+## Technologies
+* .Net Core 3.1
+* Signal R
+
+## Usage
+* Create new room rooms
+* Login into rooms with custom username
+* See your and other room participants messages
+* See typing indicator appear when 
+* To see a typing indicator please open two browsers and enter in the same room.
+
+## License
+This project is open-source under apache. You can take it and use it any way that you wish.
+
+## Live demo
+https://webchatroom.azurewebsites.net/
---
+++ src/WebChatRoom/Pages/Index.cshtml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+using WebChatRoom.Models;
+
+namespace WebChatRoom.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private const string ROOMCACHEKEY = "RoomCacheKey";
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        [BindProperty]
+        public List<Room> Rooms { get; set; }
+
+        public IndexModel(IMemoryCache memoryCache, ILogger<IndexModel> logger)
+        {
+            _memoryCache = memoryCache;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Rooms = _memoryCache.Get<List<Room>>(ROOMCACHEKEY);
+            if (Rooms == null)
+            {
+                Rooms = new List<Room>();
+                SetRoomCache(Rooms);
+            }
+        }
+
+        public IActionResult OnPostAddRoom(string roomName)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get rooms
+                this.Rooms = _memoryCache.Get<List<Room>>(ROOMCACHEKEY);
+                if (this.Rooms.Count > 9) return Page(); // limit to 10 rooms only
+
+                this.Rooms.Add(new Room() { Id = Guid.NewGuid(), Name = roomName, Participants = new List<Participant>() });
+                SetRoomCache(this.Rooms);
+            }
+            return Page();
+        }
+
+        public IActionResult OnPostJoinRoom(Guid roomId, string Name)
+        {
+            if (ModelState.IsValid)
+            {
+                // Get rooms
+                this.Rooms = _memoryCache.Get<List<Room>>(ROOMCACHEKEY);
+                var theRoom = this.Rooms.FirstOrDefault(r => r.Id == roomId);
+                if (theRoom != null)
+                {
+                    if (theRoom.Participants.Count > 9) return Page(); // limit to 10 partisipants
+                    var participantId = Guid.NewGuid();
+                    theRoom.Participants.Add(new Participant() { Id = participantId, Name = Name });
+                    SetRoomCache(this.Rooms);
+                    return new RedirectToPageResult("Room",new { roomId, participantId });
+                }
+            }
+            return Page();
+        }
+
+        private void SetRoomCache(List<Room> rooms)
+        {
+            var cacheExpirationOptions = new MemoryCacheEntryOptions()
+            {
+                AbsoluteExpiration = DateTime.Now.AddHours(5),
+                Priority = CacheItemPriority.Normal,
+                SlidingExpiration = TimeSpan.FromMinutes(10)
+            };
+            _memoryCache.Set(ROOMCACHEKEY, Rooms, cacheExpirationOptions);
+        }
+    }
+}
